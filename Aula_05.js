977f1a9db1ccbe332f3eeedb15123367895017e0
Bianca Pacheco Nunes <112512785+BiaPacNun@users.noreply.github.com>
2023-01-20 23:07:13 -0300

    Aula 05 estudos

--- Aula_05.js
+++ Aula_05.js
@@ -1,2 +1,9 @@
 // Variáveis e Tipos Primitivos
 // Link da aula: https://www.youtube.com/watch?v=Vbabsye7mWo
+
+/*Você sabe o que são variáveis? Sabe declarar variáveis em JavaScript? Sabe quais são os tipos primitivos do JavaScript? Consegue entender o que significa colocar um valor null dentro de uma variável em JavaScript?
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo.*/
+
