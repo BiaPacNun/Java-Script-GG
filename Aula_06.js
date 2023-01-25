43bad4e6b049db4aef4ace9514a4d64efac57130
Bianca Pacheco Nunes <112512785+BiaPacNun@users.noreply.github.com>
2023-01-25 00:04:36 -0300

    Aula_06 Iniciada

--- Aula_06.js
+++ Aula_06.js
@@ -1,2 +1,5 @@
 // Aula 06 - Tratamento de dados
-// https://www.youtube.com/watch?v=OJgu_KCCUSY
+// Link da aula: https://www.youtube.com/watch?v=OJgu_KCCUSY
+/* Anotações:
+Para escrever comentários no HTML é diferente do JS, se usa <!-- -->
+Pra escrever comentários no CSS é */ /* 
