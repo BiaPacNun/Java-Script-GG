646f5614798d97ade099d36cf1cbf0bbed0ef43c
Bianca Pacheco Nunes <112512785+BiaPacNun@users.noreply.github.com>
2023-02-10 17:13:30 -0300

    Aula_01 Finalizada

--- Aula_01.js
+++ Aula_01.js
@@ -2,7 +2,6 @@
 
 /*
 Você sabe para que serve a linguagem JavaScript / ECMAScript? Sabe a diferença entre um cliente e um servidor para a Internet? Sabe para que servem as tecnologias HTML5, CSS3 e JavaScript? Consegue citar 4 sites que utilizam a linguagem JavaScript nos seus códigos?
-
 Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
 */
 
